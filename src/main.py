09eb93b0ba0dcaf7c30fdb06cec271c6dfca9d92
Luis Villa <angelvilla23@gmail.com>
2017-08-14 21:49:32 -0500

    main modified D D Ci

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 import time
 import os
+from sensors_Driver import scan
 
 from notification import SendNotif
 from netcheck import net_is_up
+PIR = 7
+Pir = "PIR"
 
 while True:
-	netup = net_is_up()
-	if(1 == netup):
-		SendNotif("Test message")
-		#time.sleep(5)
+
+    Sens=scan(Pir,PIR)
+    #time.sleep(2)
+    
+    if(Sens == 1):
+		netup = net_is_up()
+		
+		if(netup ==1 ):
+			SendNotif("Test message")
+	
